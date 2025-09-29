946a6642ca466f1a546be66614f108ab33939aa0
cameron <cmonthe8@outlook.com>
2025-09-29 12:54:19 -0400

    sns creating

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -10,16 +10,16 @@ resource "aws_sns_topic" "my_topic" {
 # Create SNS Topic Subscription
 resource "aws_sns_topic_subscription" "email_subscription" {
   topic_arn = aws_sns_topic.my_topic.arn
-  protocol  = "email"  
-  endpoint  = "cmonthe8@outlook.com"  
+  protocol  = "email"
+  endpoint  = "cmonthe8@outlook.com"
 }
 
 # Output the SNS Topic ARN
 output "sns_topic_arn" {
   value = aws_sns_topic.my_topic.arn
 }
-resource  "aws_sns_topic_subscription" "sms_subscription" {
+resource "aws_sns_topic_subscription" "sms_subscription" {
   topic_arn = aws_sns_topic.my_topic.arn
   protocol  = "sms"
-  endpoint  = "2408109458" 
+  endpoint  = "2408109458"
 }
